73565017a33f757b55f8a0936221bca1833b68b0
James Kirk <jkirk@switch.tv>
2020-02-17 18:18:20 +1100

    config ports

--- packages/gateway/server.js
+++ packages/gateway/server.js
@@ -1,11 +1,31 @@
 const server = require("express")();
 const bodyParser = require("body-parser");
+const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
+
+const { port } = require("./config");
+
+const typeDefs = `
+    type Query {
+        hey: String!
+    }
+`;
+
+const resolvers = {
+  Query: {
+    hey: () => "hey there"
+  }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
 
 server
   .use(bodyParser.json())
-  .get("/", (_, res) => {
-    res.send(" I am working as expected");
-  })
-  .listen(3000, () => {
+  .use("/graphql", graphqlExpress({ schema }))
+  .use("/gq", graphiqlExpress({ endpointURL: "/graphql" }))
+  .listen(port, () => {
     console.log("listening on port 3000");
   });
